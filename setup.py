8230ab67b71f67743613c90aa26a7af35388230c
Giles Brown <giles@milo.com>
2012-11-15 13:23:34 -0800

    Support Python 2.6 string format (requires field number)

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
-from distutils.core import setup
+from os import path as p
+from setuptools import setup
+
+# from fork: https://github.com/gilesbrown/pypicache
+
+with open(p.join(p.dirname(__file__), 'requirements.txt')) as requirements:
+    install_requires = [line.strip() for line in requirements]
 
 setup(
     name='pypicache',
@@ -8,4 +14,5 @@ setup(
     author_email='mick@twomeylee.name',
     url='http://readthedocs.org/projects/pypicache/',
     packages=['pypicache'],
+    install_requires=install_requires,
 )
